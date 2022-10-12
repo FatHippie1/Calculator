90da40b6257a6f93091ff104e78329e88cf9ef0c
FatHippie1 <80604504+FatHippie1@users.noreply.github.com>
2022-10-11 21:06:39 -0600

    next

---
+++ scripts.js
@@ -0,0 +1,13 @@
+let btn1 = document.getElementById('1');
+let btn2 = document.getElementById('2');
+const equal = document.getElementById(enter);
+let result = '';
+debugger;
+function getResult() {
+    let btn1a = parseFloat(btn1.defaultValue);
+    let btn2a = parseFloat(btn2.defaultValue);
+    console.log("btn1+btn2: ", (btn1a + btn2a)
+
+    );
+}
+getResult();
