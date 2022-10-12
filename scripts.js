b5cfa95adcbc5606deead96306d9cd386eb75b61
FatHippie1 <80604504+FatHippie1@users.noreply.github.com>
2022-10-11 22:04:12 -0600

    next

--- scripts.js
+++ scripts.js
@@ -1,13 +1,19 @@
 let btn1 = document.getElementById('1');
 let btn2 = document.getElementById('2');
-const equal = document.getElementById(enter);
+let btn3 = document.getElementById('3');
+const equal = document.getElementById('enter');
 let result = '';
+
+equal.addEventListener('click', getResult);
 debugger;
 function getResult() {
     let btn1a = parseFloat(btn1.defaultValue);
     let btn2a = parseFloat(btn2.defaultValue);
-    console.log("btn1+btn2: ", (btn1a + btn2a)
+    let btn3a = parseFloat(btn3.defaultValue);
+    let result = (btn1a + btn2a + btn3a);
+    //console.log(e);
+    console.log("btn1+btn2: ", (result)
 
     );
 }
-getResult();
+//getResult();
