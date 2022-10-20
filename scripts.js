d7fe38cdf32781177db9b95ef93b575bcabf1b06
FatHippie1 <80604504+FatHippie1@users.noreply.github.com>
2022-10-20 00:20:44 -0600

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,7 +1,9 @@
 const mainDisplay = document.getElementById('display');
-let btn1 = document.getElementById('1').addEventListener('click', addNumbers);;
-let btn2 = document.getElementById('2').addEventListener('click', addNumbers);;
+let btn1 = document.getElementById('1').addEventListener('click', getFirst);;
+let btn2 = document.getElementById('2').addEventListener('click', getSecond);;
 let btn3 = document.getElementById('3').addEventListener('click', addNumbers);;
+let firstNum = '';
+let secondNum = '';
 const equal = document.getElementById('enter');
 let result = '';
 //debugger;
@@ -10,9 +12,22 @@ let result = '';
 //btn1.addEventListener('click', addNumbers);
 //btn2.addEventListener('click', addNumbers);
 //btn3.addEventListener('click', addNumbers);
+function getFirst(e) {
+    let firstNum = e.target.value;
+    console.log(firstNum);
+}
+function getSecond(e) {
+    let secondNum = e.target.value;
+    console.log(secondNum);
+}
+function addNumbers(getFirst, getSecond) {
+    let result = getFirst + getSecond;
+    console.log(result);
+    // console.log('clicked', this.value);
 
+}
 //operator event listeners
-equal.addEventListener('click', getResult);
+equal.addEventListener('click', addNumbers);
 
 //display results
 function getResult() {
@@ -26,11 +41,7 @@ function getResult() {
     mainDisplay.value = result;
 }
 //add numbers
-function addNumbers() {
-    //if btn1
-    console.log('clicked');
-
-}
+//addNumbers();
 //subract numbers
 //multiply numbers
 //divide numbers
